efe608d23d1ca849a895c117bbc181c507524592
Adit Dhanushkodi <adit.nd@gmail.com>
2023-05-13 21:42:52 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Adit's Notebook`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
